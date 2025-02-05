0406918322cc7378a4b7eb12f0278dde3411f061
Jesse Merhi <jessejmerhi@gmail.com>
2025-02-06 00:58:17 +1100

    xss demo

---
+++ xss/README.md
@@ -0,0 +1,22 @@
+
+## Notes
+Installing VENV
+What is VENV? 
+Extensions in VSCODE
+Installing Requirements
+Flask and how that works
+Flask debug mode
+
+Then we go to frontend? 
+
+Then we talk about the database?
+
+
+## Installation
+
+```shell
+python3 -m venv .venv
+source ./.venv/bin/activate
+pip3 install -r requirements.txt 
+python3 -m flask --app app run --debug
+```
---
+++ xss/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, make_response
+import db
+
+app = Flask(__name__)
+app.config['TITLE'] = 'Comments or somethn'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        db.add_comment(request.form['comment'])
+
+    query = request.args.get('q')
+    return db.get_comments(query)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5123)
